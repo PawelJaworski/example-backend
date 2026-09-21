f8fec82a315928fa8825150724d612c50d3a2bac
Pawel Jaworski <pawel.jaworski-extern@dbschenker.com>
2026-09-21 14:57:16 +0200

    branch skeleton

--- src/main/java/pl/pjaworski/examplebackend/addproductstostore/AddProductsToStoreCmd.java
+++
@@ -1,9 +0,0 @@
-package pl.pjaworski.examplebackend.addproductstostore;
-
-import java.util.List;
-import lombok.Builder;
-import pl.pjaworski.examplebackend.domain.Product;
-
-@Builder
-public record AddProductsToStoreCmd(List<Product> product) {
-}
